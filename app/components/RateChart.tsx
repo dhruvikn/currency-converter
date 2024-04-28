9992ce3cd4259195b165adebe25ce2e082590e63
Dhruvik Neharia <dhruvikn@gmail.com>
2024-04-28 14:07:44 +0530

    feat: Refactor RateChart component and add fetchExchangeRateForLastNDays function

--- app/components/RateChart.tsx
+++ app/components/RateChart.tsx
@@ -1,4 +1,40 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import type { CurrencyExchangeRatesOverAPeriodResponse } from '../helpers/api';
+import type { CurrencySymbols } from '../helpers/constants';
+
+import { fetchExchangeRateForLastNDays } from '../helpers/api';
+
 export const RateChart = () => {
+  const [exchangeRateNDays, setExchangeRateNDays] =
+    useState<CurrencyExchangeRatesOverAPeriodResponse>();
+
+  const getExchangeRateForLastNDays = async (
+    fromCurrencySymbol: CurrencySymbols = 'usd',
+    toCurrencySymbol: CurrencySymbols = 'inr',
+    days: number = 7
+  ) => {
+    const response = await fetchExchangeRateForLastNDays(
+      fromCurrencySymbol,
+      toCurrencySymbol,
+      days
+    );
+
+    if (!response || response instanceof Error) {
+      return;
+    }
+
+    if (response?.length) {
+      setExchangeRateNDays(response);
+    }
+  };
+
+  useEffect(() => {
+    getExchangeRateForLastNDays();
+  }, []);
+
   return (
     <div className="frosted-glass mt-8 rate-chart-container">
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis voluptates officiis rem, earum
--- app/helpers/api.ts
+++ app/helpers/api.ts
@@ -1,12 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import type { CurrencySymbols } from './constants';
 
 export type CurrencyExchangeResponse = {
-  [key in CurrencySymbols]: number;
+  [key in CurrencySymbols]: {
+    [key in CurrencySymbols]: number;
+  };
 } & {
   date: string;
   baseCurrency: CurrencySymbols;
 };
+
+export type CurrencyExchangeRatesOverAPeriodResponse = {
+  value: number;
+  date: string;
+}[];
 
 export const fetchExchangeRate = async (
   fromCurrencySymbol: CurrencySymbols
@@ -21,11 +28,62 @@ export const fetchExchangeRate = async (
     const response = await axios.get(CURRENCY_EXCHANGE_URL);
     const data: CurrencyExchangeResponse = response?.data?.[fromCurrencySymbol];
 
+    data.date = response.data.date;
     data.baseCurrency = fromCurrencySymbol;
 
     return data;
   } catch (error) {
     console.error(error);
     return new Error('Failed to fetch exchange rate');
+  }
+};
+
+export const fetchExchangeRateForLastNDays = async (
+  fromCurrencySymbol: CurrencySymbols,
+  toCurrencySymbol: CurrencySymbols,
+  days: number = 7
+): Promise<CurrencyExchangeRatesOverAPeriodResponse | Error> => {
+  if (!fromCurrencySymbol?.length) {
+    return new Error('Invalid currency');
+  }
+
+  const CURRENCY_EXCHANGE_URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrencySymbol}.json`;
+
+  try {
+    const fetchRequests: Promise<AxiosResponse<CurrencyExchangeResponse>>[] = [];
+
+    // Fetch exchange rates for the last 7 days
+    for (let i = 0; i < days; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() - i);
+
+      const formattedDate = date.toISOString().split('T')[0];
+      const request = axios.get(CURRENCY_EXCHANGE_URL.replace('@latest', '@' + formattedDate));
+
+      fetchRequests.push(request);
+    }
+
+    // Wait for all requests to complete
+    const responses = await Promise.all(fetchRequests);
+
+    // Process response data
+    const exchangeRates: CurrencyExchangeRatesOverAPeriodResponse = responses.map(response => {
+      const data = response?.data;
+
+      const exchangeRate = data[fromCurrencySymbol][toCurrencySymbol];
+      const date = data.date;
+
+      return {
+        value: exchangeRate,
+        date
+      };
+    });
+
+    exchangeRates.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+    return exchangeRates;
+  } catch (error) {
+    console.error(error);
+    return new Error('Failed to fetch exchange rates');
   }
 };
